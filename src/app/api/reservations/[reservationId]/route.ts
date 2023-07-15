088f4f4e1500d539f51c68fb61ee252c5bb50e3e
Fadi-N <fadinachawati17@gmail.com>
2023-07-15 18:52:31 +0200

    Cancel trip reservation

---
+++ src/app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,33 @@
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import {NextResponse} from "next/server";
+
+interface IParams {
+    reservationId?: string;
+}
+
+export async function DELETE(request: Request, {params}: { params: IParams }) {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) {
+        return NextResponse.error();
+    }
+
+    const {reservationId} = params;
+    if (!reservationId || typeof reservationId !== "string") {
+        throw new Error("Invalic ID");
+    }
+
+    const reservation = await prisma.reservation.deleteMany({
+        where:{
+            id: reservationId,
+            OR:[
+                // Only the person who made the reservation or the owner can cancel the reservation
+                {userId: currentUser.id},
+                {listing: {userId: currentUser.id}}
+            ]
+        }
+    })
+
+    return NextResponse.json(reservation)
+
+}
--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -18,7 +18,7 @@ const TripsPage = async () => {
     }
 
     const reservations = await getReservations({userId: currentUser.id})
-    if (!reservations) {
+    if (reservations.length === 0) {
         return (
             <ClientOnly>
                 <EmptyState
